6e60cd1c81d5919dfce5d4b113e6f0844bdcfd59
kukaiN <kukainakahata@gmail.com>
2020-02-10 18:11:10 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,20 +1,30 @@
 import math
 import random
+import pandas as pd
+import numpy as np
+import pickle
+import itertools
 
 class matrix():
-    def __init__(self): # initiate matrix
-        self._height = 0
-        self._width = 0
+    def __init__(self, x=2, y=2): # initiate matrix
+        self._height = y
+        self._width = x
+        self._matrix = pd.DataFrame(np.zeros((3*x,y)))
         self._square = False
         self._augmented = False
         self._fractions = False
-        self._matrix = [[]]
         self._complexValues = False
 
     def matrix_size(self, width, height):
         self._height = height
         self._width = width
         self._width = True if height == width else False
+    
+    def zero_matrix(self):
+        
+
+    def identity_matrix(self):
+        if self._height == self._width: 
 
     def modify_entry(self, i, j, value):
         if x < self._width and y < self._height and 0 < x and 0 < y:
@@ -23,12 +33,15 @@ class matrix():
             print("the index provided is not valid")
 
     def modify_row(self, row_index, row_list):
-
-        for i in range(self._height):
-            self._matrix[row_index] = 
+        if len(row_list) == self._width:
+            self._matrix[row_index] = row_list
+        else: print("list length doesnt match width of matrix")
     
-    def modify_column(self, column_index, list):
-        pass
+    def modify_column(self, column_index, column_list):
+        if len(column_list) == self._height:
+            for i in self._height:
+
+        else: print("list length doesnt match height of matrix")
     
     def LUdecompositions():
         pass
@@ -58,6 +71,7 @@ class matrix():
         pass
 
     def find_determinant():
+        pass
 
     def find_eigenvalues():
         pass
@@ -75,12 +89,28 @@ class matrix():
         pass
 
     def diagonalize():
-    
+        pass
+
     def fibonacci():
-
+        pass
     def span():
-
+        pass
     def dualSpace():
+        pass
+    def QTQ():
+        pass
+    
+    def compression():
+        pass
+    def markovchain():
+        pass
+    def stocastcic_processes():
+        pass
+    def ask_for_values(self):
+        if !(self._height > 0 and self._width > 0): 
+            while self._height < 1: self._height = int(input("please provide a valid height for the matrix: "))
+            while self._width < 1: self._width = int(input("please provide a valid width for the matrix: "))
+        
 
 def main():
     print("what is it that you want to do?")
