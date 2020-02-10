9dceded8165a1ff345965cbc945d776c5561524e
kukaiN <kukainakahata@gmail.com>
2020-02-10 14:49:55 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,33 +1,86 @@
 import math
-
-class matrix(self):
-
-
-
-
 import random
-class Robogame():
-    def __init__(self): #initialize the robot and prize
+
+class matrix():
+    def __init__(self): # initiate matrix
         self._height = 0
         self._width = 0
-        self.square = True
+        self._square = False
+        self._augmented = False
+        self._fractions = False
+        self._matrix = [[]]
+        self._complexValues = False
+
+    def matrix_size(self, width, height):
+        self._height = height
+        self._width = width
+        self._width = True if height == width else False
+
+    def modify_entry(self, i, j, value):
+        if x < self._width and y < self._height and 0 < x and 0 < y:
+            self._matrix[j][i] = value
+        else:
+            print("the index provided is not valid")
+
+    def modify_row(self, row_index, row_list):
+
+        for i in range(self._height):
+            self._matrix[row_index] = 
     
-    def challenge(self): # initialize extra variables needed for the challenge qustion
-        self.robot_position = [self._robot] # this array keeps all [x,y] of the robot because he cant go back
-        self._win1 = False
-        self._win2 = False
-        self._prize2 = [random.randint(0,5), random.randint(0,5)]
+    def modify_column(self, column_index, list):
+        pass
+    
+    def LUdecompositions():
+        pass
+
+    def GaussJordanElimination():
+        pass
 
     def multiply_matrix():
+        pass
 
     def dot_product():
+        pass
 
-    def cross_product():
-
-
+    def matrix_addition():
+        pass
     
+    def cross_product():
+        pass
 
+    def crammersRule():
+        pass
 
+    def find_inverse():
+        pass
+
+    def Transpose():
+        pass
+
+    def find_determinant():
+
+    def find_eigenvalues():
+        pass
+
+    def find_eigenvectors():
+        pass
+
+    def nullspace():    
+        pass
+    
+    def rowspace():
+        pass
+    
+    def columnspace():
+        pass
+
+    def diagonalize():
+    
+    def fibonacci():
+
+    def span():
+
+    def dualSpace():
 
 def main():
     print("what is it that you want to do?")
