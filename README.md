07334a0a523db70550fb4a554b9eb8af6ac58186
KukaiN <50426885+kukai10@users.noreply.github.com>
2020-04-21 18:36:35 -0400

    updated readme

--- README.md
+++ README.md
@@ -1,2 +1,133 @@
-# LinearAlgebra
- functions and programs that does linear algebra stuff
+# Linear Algebra using python
+
+Linear algebra algorithms and programs made with python
+
+## Important Info:
+ - Use numpy's implimentation instead of this, if performnace is a factor.
+ - the code here is to showcase the inner workings of the algorithm and is a form of note taking so that I don't forget the teachings in linear algebra.  
+
+ - main.py and test.py are test files that i use to check the validity of the code so it is separate from the linear algebra repo
+
+ - many topics in linear algebra overlaps with topics in discrete math, so if an algorithm is not in this repo, check the discrete math repo to check if its not there.
+
+## Table of contents
+- [general](#general)
+    - [Dependencies](#depen)
+    - [updates and future plans](#plans)
+- [description of files](#info)
+- 
+
+
+
+
+---
+<a id = "general"></a> 
+
+## General
+
+
+---
+<a id = "depen"></a> 
+
+## dependencies/ used libraries:
+
+## dependencies:
+- numpy (most functions have the option of using numpy and works together with matplotlib)
+- pandas (used to store data)
+- pickle (used to save data as a .pkl file)
+- matplotlib (used for visualization)
+- collections (used in few files to access stacks and queues)
+- random
+- math (used for trig functions)
+- itertools
+- fractions
+- string (used to parse string inputs)
+
+
+<a id = "plans"></a>
+
+## updates and future plans
+
+### updates:
+ - as of 4/21/2020, working on finishing the fundamental ideas in Lin-algebra, applications will be touched after everything is done. 
+
+### future plans:
+ - first I will continue and finish the files that are in the repo 
+ - Then I will go into applications in physics and modeling
+
+
+
+<a id = "info"></a> 
+
+
+---
+## file descriptions
+
+### complex_number.py
+
+
+### computer_graphics.py
+
+
+### determinant.py
+
+
+### differential_equation_with_matrix.py
+
+
+### eigenvalue.py
+
+
+### electrical_circuits.py
+
+
+### fourier_series.py
+
+
+### fractals.py
+
+
+### game_theory.py
+
+
+
+### gaussian_elimination.py
+
+
+
+### least_square_methods.py
+
+
+### linear_transformations.py
+
+
+
+### LU_decomposition.py
+
+
+
+### markov_chain.py
+
+
+
+### mathparser.py
+
+
+
+### optimization_with_quadratic_form.py
+
+
+### population_model.py
+
+
+### QR_decomposition.py
+
+
+### random_walks.py
+
+
+### singular_value_decomp.py
+
+
+
+### vector_spaces.py
--- gaussian_elimination.py
+++ gaussian_elimination.py
@@ -2,17 +2,41 @@ import numpy as np
 import fractions
 
 
-def gaussian_elimination(matrix_A, augmented=True, RREF=True):
+def gaussian_elimination(matrix_A, augmented=False, square_matrix=True, RREF=True):
+    # gaussian jordan is O(n^3)
     # RREF: Reduced Row Echelon Form
     # if RREF is False then it does row reduction until REF
-    if augmented:
-        for i in range len(matrix_A): # go through each row, and do reow reduction
-            # row reduction
+    # make sure that the matrix is not sparce, meaning dont use this for matrices that have specialized algorithms    
+    row_num = len(matrix_A)
+    col_len = len(matrix_A[0]) 
+
+    # forward elimination
+    for pivot_ij in range(row_num):
+        current_pivot = matrix_A[pivot_ij][pivot_ij]
+        pivot_num, pivot_denom = current_pivot.numerator, current_pivot.denominator
+        flipped_pivot = fractions.Fraction(numerator=pivot_denom, denominator=pivot_nu
+        m)
+        for j in range(pivot_ij, col_len):
+            matrix_A[pivot_ij][j]*=flipped_pivot
+    if square_matrix == False or RREF == False: # return REF (not unique)
+        return matrix_A
+    
+    # backward elimination
+
+    for pivot_ij in reversed(range(1, row_num)):
+        for col_index in range(pivot_ij, row_num):
+            next_to_pivot = 
 
 
-            if RREF: # keep on going and reduce it until 
-                pass
-
+    return matrix_A
+    
+    
+        
+def print_matrix(matrix):
+    for row in matrix:
+        print([frac if frac.denominator != 1 else frac.numerator for frac in row])
+    print("*" * 20)
+        
 
 
 def gaussian_elimination_return_transformation(matrix_A, augmented=True, RREF=True):
@@ -21,7 +45,29 @@ def gaussian_elimination_return_transformation(matrix_A, augmented=True, RREF=Tr
     matrix_A = [[fractions.fractions(value) for value in row] for row in matrix_A]
     if augmented:
         for i in range(len(matrix_A)):
+            pass
             # row reduction 
             # and store the simple transfrmation 
 
     # return transformation
+
+def convert_to_fractions(matrix):
+    # returns the matrix but convert all entries into fractional object to reduce the amount of error
+    # runtime O(size of matrix) = O(n^2)
+    return [[fractions.Fraction(entry) for entry in row] for row in matrix]
+
+
+def main():
+    matrix = [[1, 2, 3, 4],
+            [0, 6, 7, 8],
+            [0, 0, 11, 12],
+            [0, 0, 0, 16]]
+
+    matrix = convert_to_fractions(matrix)
+    gauss = gaussian_elimination(matrix, augmented=False, square_matrix=True, RREF=True)
+    print_matrix(gauss)
+
+
+
+if __name__ == "__main__":
+    main()
